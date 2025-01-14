e877ff894898b17abecbab18ddcc6caedaedbf45
ahmed adel <ahmed.adel.dev@hotmail.com>
2025-01-14 20:36:39 +0200

    restored database

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ipbpsxhqaiwzifejhybb.supabase.co",
+        hostname: "pzqtyaoyekahgjwsrnzh.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
